53719d63dab68f51563a1344bfa9846a0d29a71d
isra <israelkouperman@gmail.com>
2021-02-04 08:19:34 +0200

    change read to byte per byte

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 
 
 const buf = fs.readFileSync("files/one.gpx");
-console.log(new Uint8Array(buf));
 
 
 const byteLength = buf.byteLength;
@@ -11,38 +10,42 @@ console.log("Size: " + (byteLength / 1000) + "kb")
 const header = new Uint8Array(buf.slice(0, 4), 0, 4);
 
 
-const decompressedDataLength = buf.readInt32LE(4);
+// const decompressedDataLength = buf.readInt32LE(4);
+const decompressedDataLength = buf.readIntLE(4, 32);
 console.log(decompressedDataLength);
 
 console.log(header + " - uncompressed length: " + decompressedDataLength);
 
-const dataBuf = buf.subarray(7, byteLength);
+
+const dataBuf = buf.subarray(36, byteLength);
 const dataBufLength = dataBuf.byteLength;
 
-let byteOffset = 0;
-while (byteOffset < dataBufLength) {
-    const compressionBit = dataBuf.readUInt8(byteOffset);
-    if (compressionBit !== 1 && compressionBit !== 0) {
-        throw new Error("error not a compression bit");
-    }
-    const isCompressed = compressionBit === 1;
-    console.log(`CHUNK:${byteOffset}, C:${isCompressed}`)
-    if (isCompressed) {
-        const lengthBit = dataBuf.readUInt32BE(byteOffset++);
-        const lengthBitExtracted = lengthBit >>> 28;
-        console.log("CHUNK:bytes: " + lengthBitExtracted);
+const decompressedBitArray = [];
 
+let bitOffset = 0;
+while (bitOffset < dataBufLength) {
+    const compressionBit = dataBuf.readIntLE(bitOffset, 1);
+    bitOffset++;
+    console.log("BLOCK:"+compressionBit);
+    if (compressionBit) {
+        console.log(`CHUNK:${bitOffset}, C:${compressionBit}`)
+        const lengthBit = dataBuf.readIntLE(bitOffset, 2);
+        console.log(`CHUNK:${bitOffset}, L:${lengthBit}`)
+        bitOffset += 2;
+        const rawData = dataBuf.readIntLE(bitOffset, lengthBit);
+        decompressedBitArray.push(rawData);
+        bitOffset += lengthBit;
     } else {
-        byteOffset++;
-        const lengthBit = dataBuf.readUInt32LE(byteOffset);
-        console.log(lengthBit.toString(2));
-        const lengthBitExtracted = lengthBit >>> 30;
+        console.log(`CHUNK:${bitOffset}, C:${compressionBit}`);
+
+        const lengthBit = dataBuf.readIntLE(bitOffset, 4);
+        console.log(`CHUNK:${bitOffset} L:${lengthBit.toString(2)}` );
         console.log("CHUNK:bytes: " + lengthBitExtracted);
 
         const chunkByteArray = [];
         for (let i = 0; i < lengthBitExtracted; i++) {
-            byteOffset++;
-            chunkByteArray.push(dataBuf.readUInt8(byteOffset))
+            bitOffset++;
+            chunkByteArray.push(dataBuf.readUInt8(bitOffset))
         }
         console.log("CHUNK:data " + chunkByteArray)
     }
